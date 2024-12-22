da935ea47d5a85a3c066636f74bf0458ede25e7b
1txb1l4l <mbilal2913@gmail.com>
2024-12-22 15:50:08 +0500

    add theme powerk10

--- setup/linux/ohmyzsh.sh
+++ setup/linux/ohmyzsh.sh
@@ -23,6 +23,7 @@ plugins() {
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 	git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+	git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 }
 
 # Main function to orchestrate the process
