23d4b284f1c2253e9481d58d5ad449ebf37c24eb
1txb1l4l <mbilal2913@gmail.com>
2025-01-20 20:58:26 +0500

    add zsh as default

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,30 +1,37 @@
 return {
-	"akinsho/toggleterm.nvim",
-	version = "*",
-	config = function()
-		require("toggleterm").setup({
-			size = 20, -- Terminal size for horizontal/vertical splits
-			open_mapping = [[<C-t>]], -- Keybinding to open the terminal
-			hide_numbers = true, -- Hide line numbers in terminal buffer
-			shade_filetypes = {}, -- Filetypes to shade
-			shade_terminals = true, -- Enable shading for terminals
-			shading_factor = 2, -- Darkening factor for terminal background
-			start_in_insert = true, -- Start terminal in insert mode
-			insert_mappings = true, -- Apply terminal keybindings in insert mode
-			persist_size = true, -- Preserve terminal size across toggles
-			direction = "horizontal", -- Terminal direction: 'horizontal', 'vertical', 'float', or 'tab'
-			close_on_exit = true, -- Close terminal when the process exits
-			shell = vim.o.shell, -- Use the default shell
-			float_opts = {
-				border = "single", -- Border style: 'single', 'double', 'curved', etc.
-				width = 100, -- Width of the floating terminal
-				height = 30, -- Height of the floating terminal
-				winblend = 3, -- Transparency of the floating terminal
-				highlights = {
-					border = "Normal", -- Highlight for the border
-					background = "Normal", -- Highlight for the background
-				},
-			},
-		})
-	end,
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+        require("toggleterm").setup({
+            size = 20,
+            open_mapping = [[<C-t>]], -- This is already set for Control + t
+            hide_numbers = true,
+            shade_filetypes = {},
+            shade_terminals = true,
+            shading_factor = 2,
+            start_in_insert = true,
+            insert_mappings = true,
+            persist_size = true,
+            direction = "horizontal",
+            close_on_exit = true,
+            shell = "/bin/zsh", -- Set zsh as default shell
+            float_opts = {
+                border = "single",
+                width = 100,
+                height = 30,
+                winblend = 15, -- Increased transparency (0-100, where 100 is fully transparent)
+                highlights = {
+                    border = "Normal",
+                    background = "Normal",
+                },
+            },
+            -- Add background transparency for non-floating terminals
+            highlights = {
+                Normal = {
+                    guibg = "NONE", -- This makes the background transparent
+                    ctermbg = "NONE"
+                },
+            },
+        })
+    end,
 }
