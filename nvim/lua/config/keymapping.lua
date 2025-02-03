351f33ebdd80f5febafb0d1ff21f9fcea47de0c1
1txb1l4l <mbilal2913@gmail.com>
2025-02-04 04:01:10 +0500

    add keymapping - Toggleterm

--- nvim/lua/config/keymapping.lua
+++ nvim/lua/config/keymapping.lua
@@ -20,3 +20,9 @@ vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap
 vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope git_files<CR>', { noremap = true, silent = true })
+
+-- Toggleterm
+vim.api.nvim_set_keymap('n', '<leader>t1', ':ToggleTerm1<CR>', { noremap = true, silent = true, desc = "Toggle Terminal 1" })
+vim.api.nvim_set_keymap('n', '<leader>t2', ':ToggleTerm2<CR>', { noremap = true, silent = true, desc = "Toggle Terminal 2" })
+vim.api.nvim_set_keymap('n', '<leader>t3', ':ToggleTerm3<CR>', { noremap = true, silent = true, desc = "Toggle Terminal 3" })
+vim.api.nvim_set_keymap('n', '<leader>t4', ':ToggleTerm4<CR>', { noremap = true, silent = true, desc = "Toggle Terminal 4" })
