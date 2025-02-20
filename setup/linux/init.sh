0c0eefab4f3cc7a3f196812cd07b2720fed9a222
1txb1l4l <mbilal2913@gmail.com>
2025-02-20 20:36:38 +0500

    change it for codespace

--- setup/linux/init.sh
+++ setup/linux/init.sh
@@ -9,6 +9,7 @@ echo "Permissions Granted."
 echo "Running all scripts..."
 . ~/MyDot/setup/linux/packages.sh
 . ~/MyDot/setup/linux/ohmyzsh.sh
+. ~/MyDot/setup/linux/pluginzs.sh
 . ~/MyDot/setup/linux/nvim.sh
 
 # Python Scripts
@@ -16,28 +17,3 @@ echo "Running Python scripts..."
 python3 ~/MyDot/setup/linux/nvim-set.py
 python3 ~/MyDot/setup/linux/zsh-setup.py
 
-# Git Values
-check_git_config() {
-    local username=$(git config --global user.name)
-    local email=$(git config --global user.email)
-
-    echo "Checking Git configuration..."
-    if [ -n "$username" ] && [ -n "$email" ]; then
-        echo "Git is already configured with:"
-        echo "Username: $username"
-        echo "Email: $email"
-    else
-        echo "Git username and/or email are not configured."
-        if confirm_step "Do you want to configure Git now?"; then
-            read -p "Enter your Git username: " new_username
-            read -p "Enter your Git email: " new_email
-
-            run_command "git config --global user.name \"$new_username\"" "Setting Git username"
-            run_command "git config --global user.email \"$new_email\"" "Setting Git email"
-        else
-            echo "Skipped Git configuration."
-        fi
-    fi
-}
-
-check_git_config
--- setup/linux/ohmyzsh.sh
+++ setup/linux/ohmyzsh.sh
@@ -2,27 +2,38 @@
 
 OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
 
-# Function to delete the .oh-my-zsh directory if it exists
+# Function to remove .oh-my-zsh if it exists
 remove_oh_my_zsh() {
-    echo "Checking if $OH_MY_ZSH_DIR exists..."
     if [ -d "$OH_MY_ZSH_DIR" ]; then
         echo "Deleting existing .oh-my-zsh directory..."
-        rm -rf "$OH_MY_ZSH_DIR" && echo ".oh-my-zsh deleted successfully." || echo "Failed to delete .oh-my-zsh."
+        rm -rf "$OH_MY_ZSH_DIR" && echo ".oh-my-zsh deleted successfully."
     else
         echo ".oh-my-zsh directory does not exist, skipping deletion."
     fi
 }
 
-install() {
-	echo "installing oh-my-zsh"
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Function to install Oh My Zsh
+install_oh_my_zsh() {
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
-# Main function to orchestrate the process
+# Function to check if Oh My Zsh is installed
+is_installed() {
+    if [ -d "$OH_MY_ZSH_DIR" ]; then
+        return 0  # Oh My Zsh is installed
+    else
+        return 1  # Oh My Zsh is not installed
+    fi
+}
+
+# Main function
 main() {
-    remove_oh_my_zsh
-    install
-    plugins
+    if is_installed; then
+        echo "Oh My Zsh is already installed. Skipping installation."
+    else
+        install_oh_my_zsh
+    fi
 }
 
 # Execute the main function
