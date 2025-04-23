b4250e693e003535291fff520a2452ddcf107417
ByteMorphic <71386745+1TXB1L4L@users.noreply.github.com>
2025-04-23 22:42:33 +0500

    add options and customize accourding to need

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+vim.opt.number = true          -- Show line numbers
+vim.opt.relativenumber = true  -- Show relative line numbers
+vim.opt.tabstop = 2            -- Tab width
+vim.opt.shiftwidth = 2         -- Indentation width
+vim.opt.expandtab = true        -- Convert tabs to spaces
+vim.opt.smartindent = true    -- Smart indentation
+vim.opt.autoindent = true     -- Auto indent new lines
+vim.opt.wrap = true          -- Enable line wrapping
+vim.opt.cursorline = true    -- Highlight the current line
+
+
