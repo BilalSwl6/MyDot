88dd791e827e156c136060dd79cd8678da59cb57
ByteMorphic <71386745+1TXB1L4L@users.noreply.github.com>
2025-04-23 15:10:04 +0500

    add details of plugins, modify and add details

--- nvim/lua/config/keymappings.lua
+++ nvim/lua/config/keymappings.lua
@@ -2,24 +2,25 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "/"
 
 -- nvim-tree
-vim.api.nvim_set_keymap('n', '<leader>ee', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ee', ':NvimTreeToggle<CR>', { noremap = true, silent = true, desc = "Toggle Nvim Tree" })
 vim.keymap.set('n', '<leader>tc', function()
   require('nvim-tree.api').tree.collapse_all()
 end, { desc = "Fold all folders in Nvim Tree" })
 
 -- make tabs easier to navigatete
-vim.api.nvim_set_keymap('n', '<leader>tp', ':tabprevious<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>tn', ':tabnext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>tx', ':tabclose<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>to', ':tabnew<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tp', ':tabprevious<CR>', { noremap = true, silent = true, desc = "Previous Tab" })
+vim.api.nvim_set_keymap('n', '<leader>tn', ':tabnext<CR>', { noremap = true, silent = true, desc = "Next Tab" })
+vim.api.nvim_set_keymap('n', '<leader>tx', ':tabclose<CR>', { noremap = true, silent = true, desc = "Close current tab" })
+vim.api.nvim_set_keymap('n', '<leader>to', ':tabnew<CR>', { noremap = true, silent = true, desc = "Open new tab" })
+vim.api.nvim_set_keymap('n', '<leader>ts', ':split<CR>', { noremap = true, silent = true, desc = "Split Window" })
 
 
 -- telescope
-vim.api.nvim_set_keymap('n', '<leader>fa', ':Telescope find_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope git_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fa', ':Telescope find_files<CR>', { noremap = true, silent = true, desc = "Find Files" })
+vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true, desc = "Live Grep" })
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true, desc = "Find Buffers" })
+vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true, desc = "Find Help Tags" })
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope git_files<CR>', { noremap = true, silent = true, desc = "Find Git Files" })
 
 -- Toggleterm
 vim.api.nvim_set_keymap('n', '<leader>t1', ':ToggleTerm1<CR>', { noremap = true, silent = true, desc = "Toggle Terminal 1" })
