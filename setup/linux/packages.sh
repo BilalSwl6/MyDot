1a1f2534ad644e0606bafcf700ad94b151f47e07
BilalSwl6 <mbilal2913@gmail.com>
2025-07-23 18:45:48 +0500

    improve

--- setup/linux/packages.sh
+++ setup/linux/packages.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen -y
-
-
-sudo apt-get install zsh -y
 
 sudo apt update && sudo apt install software-properties-common -y
 
 
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
+source ~/.zshrc
 
 
 # Download and install nvm:
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+
+
+source ~/.zshrc
--- setup/linux/pluginzsh.sh
+++ setup/linux/pluginzsh.sh
@@ -5,10 +5,10 @@ install_plugins() {
     echo "Installing plugins..."
     local ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
     
-    git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-    git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
-    git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+    git clone https://github.com/zsh-users/zsh-autosuggestions --depth=1 $ZSH_CUSTOM/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git --depth=1 $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+    git clone https://github.com/MichaelAquilina/zsh-you-should-use.git --depth=1 $ZSH_CUSTOM/plugins/you-should-use
+    git clone https://github.com/romkatv/powerlevel10k.git --depth=1 $ZSH_CUSTOM/themes/powerlevel10k
 }
 
 # Call the function to install plugins
